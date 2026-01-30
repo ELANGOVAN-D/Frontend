7d0b463e20cb6e12b185ced3cf62a86c8b7fa46a
elangovand7666 <elangovand.22csd@kongu.edu>
2026-01-30 15:13:10 +0530

    Add project files.

---
+++ Components/Model/MasterModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Frontend.Components.Model
+{
+    public class AppSettings
+    {
+        public string url { get; set; }
+    }
+    public class MasterModel
+    {
+    }
+
+    public class Student
+    {
+        public int SID { get; set; }
+        public string NickName { get; set; }
+        public string Name { get; set; }
+        public string RollNo { get; set; }
+        public int DeptCode { get; set; }
+        public string MobileNo { get; set; }
+        public string EmailiD { get; set; }
+
+
+    }
+}
